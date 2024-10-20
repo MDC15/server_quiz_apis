80c53c6e86cd299f44a1dc3b335a7aef1efd0544
MDC <120914873+MDC15@users.noreply.github.com>
2024-10-20 18:55:26 +0700

    Update api_server.py

--- api_server.py
+++ api_server.py
@@ -36,4 +36,4 @@ async def get_ranking():
 
 
 def run_fastapi():
-    uvicorn.run(app, host="127.0.0.1", port=3080)
+    uvicorn.run(app, host="https://serverquizapis-gvqxqkov76pqp3qpxaey97.streamlit.app/", port=8080)
